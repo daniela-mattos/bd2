8e7c65a8241606f016eb458acc78152e036a052d
Daniela Mattos <131220332+daniela-mattos@users.noreply.github.com>
2024-10-14 09:27:34 -0300

    Update exercicio_view.sql

--- exercicio_view.sql
+++ exercicio_view.sql
@@ -7,7 +7,8 @@ from curso
 where turno = 'Noite'
 WITH CHECK OPTION;
 
-insert into vcursos values(s_id_curso.nextval,'Biologia','Noite',6);
+insert into vcursos values(s_id_curso.nextval,'Biologia','Manhã',6); //não adiciona
+  //não deleta item que não seja turno 'Noite'
 
 
 /* 2 - Criar uma view com alguma função como sum, count ou avg e verificar de aceita algum comando DML como 
@@ -18,7 +19,7 @@ select id_turma, AVG(media_final) as media
 from turma_aluno
 group by id_turma;
 
-insert into vmedia_alunos values(23, 6);
+insert into vmedia_alunos values(23, 6);// Não funciona nenhum comando DML porque o campo "total" não existe na tabela
 
 /* 3 - Criar uma view com inner join que permita a utilização de dois comandos DML
 */
@@ -26,7 +27,7 @@ create or replace view varea_curso as
 select a.nome_area, c.nome_curso
 from area a inner join curso c
 on a.id_area = c.id_area
-
+//por mostrar dados de duas tabelas diferentes, não aceita comandos DML, principalmente insert.
 
 /* 4 - Criar uma view que apresenta o nome dos alunos que foram aprovados em todas as disciplinas do curso 1.
 - tentando (fazer usando as quantidades) */
@@ -39,7 +40,10 @@ on ta.id_turma = t.id_turma
 inner join disciplina d
 on t.id_disciplina = d.id_disciplina
 where ta.media_final >= 7 and d.id_curso = 1
-group by a.nome_aluno;
+group by a.nome_aluno
+  having count(*) = (select count(*)
+  from disciplina
+  where id_curso=1);
 
 select * from aluno
 select * from varea_curso
